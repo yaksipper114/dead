b5468957f90d8cd2143df38aea7f3af0687f5d13
yaksipper114 <haha53raid@gmail.com>
2023-06-25 15:22:24 -0400

    Initial Commit

---
+++ RallyUp/RallyUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RallyUpApp.swift
+//  RallyUp
+//
+//  Created by Ben Fichter on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RallyUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
